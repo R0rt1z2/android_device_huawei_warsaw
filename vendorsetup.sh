f14be0ea93619e7b2226faf08f4b7f40d1648ef7
DarkDroidDev <darkdroid.dev@gmail.com>
2020-06-09 02:12:35 +0200

    warsaw: First call from hi6250-common (TWRP)

---
+++ vendorsetup.sh
@@ -0,0 +1,18 @@
+#
+# Copyright (C) 2019-2020 OrangeFox Recovery Project
+#
+# This software is licensed under the terms of the GNU General Public
+# License version 2, as published by the Free Software Foundation, and
+# may be copied, distributed, and modified under those terms.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# See <http://www.gnu.org/licenses/>.
+#
+# Please maintain this if you use this script or any part of it
+#
+
+add_lunch_combo omni_warsaw-eng
